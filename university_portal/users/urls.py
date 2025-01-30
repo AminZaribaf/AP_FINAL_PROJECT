d5ad4eca2318f8efe5101158321861ec42871c7b
Amin <mohammad1383620@gmial.com>
2025-01-30 14:55:01 +0330

    i fix urls

--- university_portal/users/urls.py
+++ university_portal/users/urls.py
@@ -1,4 +1,11 @@
+from django.urls import path
+from .views import RegisterUserView, LoginUserView, PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
-
+    #
+    path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('login/', LoginUserView.as_view(), name='login_user'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
+
